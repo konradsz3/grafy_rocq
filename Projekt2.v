3d9d087d97114400f62a609a489883abd7993f56
KlemczakA <146941802+KlemczakA@users.noreply.github.com>
2026-06-07 19:19:02 +0200

    Wierzchołki i krawędzie grafu K33

--- Projekt2.v
+++ Projekt2.v
@@ -8,3 +8,28 @@ Record Graf := {
   bezPetli : forall v : nat, krawedzie v v = false;
   dobrzeZdef : forall u v : nat, krawedzie u v = true -> In u wierzcholki /\ In v wierzcholki
 }.
+
+Definition wierzcholkiK33 := 1 :: 2 :: 3 :: 4 :: 5 :: 6 :: nil.
+
+Definition krawedzieK33 (v : nat) (u : nat) : bool :=
+  match v, u with
+  | 1, 4 => true
+  | 1, 5 => true
+  | 1, 6 => true
+  | 2, 4 => true
+  | 2, 5 => true
+  | 2, 6 => true
+  | 3, 4 => true
+  | 3, 5 => true
+  | 3, 6 => true
+  | 4, 1 => true
+  | 4, 2 => true
+  | 4, 3 => true
+  | 5, 1 => true
+  | 5, 2 => true
+  | 5, 3 => true
+  | 6, 1 => true
+  | 6, 2 => true
+  | 6, 3 => true
+  | _, _ => false
+  end.
